2f4d078cd4d632b0cba53c68259ebbd597b68407
neilalexanderlee <neilalexanderlee@foxmail.com>
2017-04-18 11:13:56 +0800

    修改配置文件

--- src/test/java/com/group/test/TestJunit.java
+++ src/test/java/com/group/test/TestJunit.java
@@ -2,11 +2,8 @@ package com.group.test;
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,8 +16,8 @@ import com.group.service.IUserService;
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
 
-public class TestJUnit {
-    private static Logger logger = Logger.getLogger(TestJUnit.class);
+public class TestJunit {
+    private static Logger logger = Logger.getLogger(TestJunit.class);
     // private ApplicationContext ac = null;
     @Resource
     private IUserService userService = null;
